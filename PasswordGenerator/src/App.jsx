a6a47f76fef38ef2c124a5190d7bf7730bfa206a
ParagNukte <paragnuktek@gmail.com>
2023-11-19 20:35:24 +0530

    Added PassGenHooks

---
+++ PasswordGenerator/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import PassGen from './components/PassGen'
+import PassGenHooks from './components/PassGenHooks'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <PassGenHooks/>
+    </div>
+  )
+}
+
+export default App
