2ced39abfbc9ef43bc7f54e01e10d2a2df8392c8
ParagNukte <paragnuktek@gmail.com>
2024-03-19 20:42:03 +0530

    Added: Update feature Fix:CSS

--- React_Redux/React_Redux/src/components/AddTodo.jsx
+++ React_Redux/React_Redux/src/components/AddTodo.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
-function AddTodo() {
+import "./Todos.css"; // Import your CSS file
 
+function AddTodo() {
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
 
@@ -10,20 +11,24 @@ function AddTodo() {
     e.preventDefault();
     dispatch(addTodo(input));
     setInput("");
-    
   };
+
   return (
     <>
-      <form onSubmit={addTodoHandler}>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button type="submit">Enter todo</button>
+      <form onSubmit={addTodoHandler} className="add-todo">
+        <div className="input-container">
+          <input
+            className="addTodoInput"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button id="submitButton" type="submit">Enter todo</button>
+        </div>
       </form>
     </>
   );
 }
+
 
 export default AddTodo;
--- React_Redux/React_Redux/src/components/Todos.jsx
+++ React_Redux/React_Redux/src/components/Todos.jsx
@@ -1,18 +1,53 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlice";
+import { removeTodo, updateTodo } from "../features/todo/todoSlice";
+
 function Todos() {
+  const [editTodoId, setEditTodoId] = useState(null);
+  const [isEdited, setIsEdited] = useState(false);
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
+
+  const onChangeText = (id, newText) => {
+    dispatch(updateTodo({ id, text: newText }));
+  };
+
+  const handleEdit = (id) => {
+    setEditTodoId(id);
+    setIsEdited(!isEdited);
+  };
+
   return (
-    <div>
-      <ul>
+    <div className="todos-container">
+      
+      <ul className="todos-list">
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} className="todo-item">
             <div>
-              {todo.text}{" "}
-              <button onClick={()=>dispatch(removeTodo(todo.id))}>
-                Delete
-              </button>
+              {editTodoId === todo.id && isEdited ? (
+                <input
+                  type="text"
+                  value={todo.text}
+                  onChange={(e) => onChangeText(todo.id, e.target.value)}
+                  className="edit-input"
+                />
+              ) : (
+                <span className="todo-text">{todo.text}</span>
+              )}
+              <span className="feature-buttons">
+                {isEdited && editTodoId === todo.id ? (
+                  <button className="save-button" onClick={() => setIsEdited(false)}>
+                    Save
+                  </button>
+                ) : (
+                  <button className="edit-button" onClick={() => handleEdit(todo.id)}>
+                    Edit
+                  </button>
+                )}
+                <button className="delete-button" onClick={() => dispatch(removeTodo(todo.id))}>
+                  Delete
+                </button>
+              </span>
             </div>
           </li>
         ))}
